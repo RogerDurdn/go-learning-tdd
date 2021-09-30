cc42f225bcc3827d9d04d7a6f1f79a9d280dc208
Roger Velazquez <kumotodeas@hotmail.com>
2021-09-30 10:55:22 -0500

    comit oneee

---
+++ chapterOne/helloWeb.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hello, my name is Roger Drdn")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe("localhost:4000", nil)
+}
