b1b1548b26134899a2c391e3b99842a48cf1c706
Roger Velazquez <kumotodeas@hotmail.com>
2021-10-03 22:23:39 -0500

    initialization of the last part of chapter one

---
+++ chapterOne/3-goRoutines/runAnEcho/echoRutine.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	go echo(os.Stdin, os.Stdout)
+	time.Sleep(10 * time.Second)
+	fmt.Println("Time out. mothafuka")
+	os.Exit(0)
+}
+
+func echo(in io.Reader, out io.Writer) {
+	io.Copy(out, in)
+
+}
