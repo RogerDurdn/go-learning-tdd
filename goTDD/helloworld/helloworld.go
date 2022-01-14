c315f62b64c641641282d7317c7d083f8fb449ad
rogerdurdn <rogervelazquez@Rogers-MacBook-Air.local>
2022-01-13 21:12:59 -0600

    adding test to hello world

--- goTDD/helloworld/helloworld.go
+++ goTDD/helloworld/helloworld.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+const englishHelloWord = "hello"
+
 func main() {
  fmt.Println(hello())
+ fmt.Println(helloRecipient("world"))
+ fmt.Println(helloDefault(""))
+ fmt.Println(helloDefault("roger"))
 }
 
 func hello() string {
- return "hello world"
+ return englishHelloWord + " world"
 }
 
 func helloRecipient(recipient string) string {
- return "hello " + recipient
+ return englishHelloWord +" "+ recipient
+}
+
+func helloDefault(recipient string) string {
+ if recipient == "" {
+  recipient = "world"
+ }
+ return englishHelloWord +" "+ recipient
 }
--- goTDD/helloworld/helloworld_test.go
+++ goTDD/helloworld/helloworld_test.go
@@ -9,10 +9,49 @@ func TestHello(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
 func TestHelloWithRecipient(t *testing.T) {
 	got := helloRecipient("recipient")
 	want := "hello recipient"
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
+}
+
+func TestHelloDefault(t *testing.T) {
+	t.Run("hello with default", func(t *testing.T) {
+		got := helloDefault("")
+		want := "hello world"
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+	t.Run("hello with recipient", func(t *testing.T) {
+		got := helloDefault("roger")
+		want := "hello roger"
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+// using a function to no repeat code
+// refactored to use a common function
+func TestAssertDefault(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+	t.Run("hello with default", func(t *testing.T) {
+		got := helloDefault("")
+		want := "hello world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("hello with recipient", func(t *testing.T) {
+		got := helloDefault("roger")
+		want := "hello roger"
+		assertCorrectMessage(t, got, want)
+	})
 }
