f330061d5afbe2cf5286605dae6ac7597c9e082d
rogerdurdn <therogerdeas@hotmail.com>
2022-01-19 18:20:40 -0600

    injectiton to io writer

--- goTDD/injection/injection.go
+++ goTDD/injection/injection.go
@@ -1,7 +1,23 @@
-package injection
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
-func Greet(name string) {
-	fmt.Printf("hello %s", name)
+func main() {
+	Greet(os.Stdout, "Some")
+	http.HandleFunc("/", MyGreetHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, r.URL.Query().Get("name"))
+}
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "hello %s", name)
 }
--- goTDD/injection/injection_test.go
+++ goTDD/injection/injection_test.go
@@ -1,1 +1,18 @@
-package injection
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Roger")
+
+	got := buffer.String()
+	want := "hello Roger"
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+}
