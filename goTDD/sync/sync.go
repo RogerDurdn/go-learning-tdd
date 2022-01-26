91a47a3606b7064c7f80ad744861b0ba98a9897f
Roger Drdn <therogerdeas@hotmail.com>
2022-01-26 12:32:15 -0600

    adding async and context initial

--- goTDD/sync/sync.go
+++ goTDD/sync/sync.go
@@ -1,4 +1,25 @@
 package main
 
+import "sync"
+
 func main() {
+}
+
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+func (c *Counter) Value() int {
+	return c.count
 }
--- goTDD/sync/sync_test.go
+++ goTDD/sync/sync_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times least it at 3", func(t *testing.T) {
+		counter := NewCounter()
+
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrency", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("Error counting: got %v, want %v", got.Value(), want)
+	}
+
+}
