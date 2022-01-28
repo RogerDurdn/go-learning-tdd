4ad54b1b627457d7827c7949515c7dc05634cac1
rogerdurdn <therogerdeas@hotmail.com>
2022-01-28 10:24:42 -0600

    adding

--- goTDD/maths/maths_test.go
+++ goTDD/maths/maths_test.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"projectsBook/goTDD/maths/clockface"
 	"testing"
-	"time"
 )
 
 func TestSecondHandAt30Seconds(t *testing.T) {
-	clockTime := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(clockTime)
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
 }
