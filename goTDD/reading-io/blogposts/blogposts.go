07172516e59c8165df23f65d58e5389779cc372a
Roger Drdn <therogerdeas@hotmail.com>
2022-01-28 19:06:38 -0600

    progress

--- goTDD/reading-io/blogposts/blogposts.go
+++ goTDD/reading-io/blogposts/blogposts.go
@@ -1,4 +1,16 @@
-package main
+package blogpost
 
-func main() {
+import (
+	"io/fs"
+)
+
+type Post struct{}
+
+func NewPostFromFS(fileSystem fs.FS) []Post {
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts
 }
--- goTDD/reading-io/blogposts/blogposts_test.go
+++ goTDD/reading-io/blogposts/blogposts_test.go
@@ -1,1 +1,21 @@
-package main
+package blogpost_test
+
+import (
+	blogpost "projectsBook/goTDD/reading-io/blogposts"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPost(t *testing.T) {
+	fsystemTest := fstest.MapFS{
+		"hello word.md":  {Data: []byte("hi")},
+		"hello-word2.md": {Data: []byte("hola")},
+	}
+	posts := blogpost.NewPostFromFS(fsystemTest)
+	got := len(posts)
+	want := len(fsystemTest)
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
