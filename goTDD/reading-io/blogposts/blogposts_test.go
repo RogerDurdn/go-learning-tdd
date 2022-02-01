3f3dac7cf699d8b38b5350248f9966f9a297a939
rogerdurdn <therogerdeas@hotmail.com>
2022-01-31 21:49:05 -0600

    adding more fro io not implementing the buffer spec

--- goTDD/reading-io/blogposts/blogposts_test.go
+++ goTDD/reading-io/blogposts/blogposts_test.go
@@ -7,14 +7,27 @@ import (
 )
 
 func TestNewBlogPost(t *testing.T) {
-	fsystemTest := fstest.MapFS{
-		"hello word.md":  {Data: []byte("hi")},
-		"hello-word2.md": {Data: []byte("hola")},
+	const (
+		firstBody = `Title: post 1
+Description: description 1`
+		secondBody = `Title: post 2
+Description: description 2`
+	)
+	systemTestMap := fstest.MapFS{
+		"hello word.md":  {Data: []byte("Title: post 1")},
+		"hello-word2.md": {Data: []byte("Title: post 2")},
 	}
-	posts := blogpost.NewPostFromFS(fsystemTest)
-	got := len(posts)
-	want := len(fsystemTest)
+	posts, err := blogpost.NewPostFromFS(systemTestMap)
+	if err != nil {
+		t.Fatalf("Error on newPost: %v", err)
+	}
+	assertPost(t, posts[0], blogpost.Post{Title: "post 1",
+		Description: "description 1",
+		})
+}
 
+func assertPost(t *testing.T, got blogpost.Post, want blogpost.Post)  {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
